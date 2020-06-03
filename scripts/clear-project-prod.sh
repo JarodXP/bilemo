8440e848b53cc6fd7da200e76d0c5a07522a44ba
jarod <gregory@jarod-xp.com>
2020-06-03 09:05:50 +0200

    Fix clear-project script doesn't remove hidden files

--- scripts/clear-project-prod.sh
+++ scripts/clear-project-prod.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+## Removes all files and subfolders ##
 rm -rf /var/www/bilemo/*
+
+## Find all hidden files and delete it ##
+find /var/www/bilemo/ -maxdepth 1 -type f -name ".*" -delete
