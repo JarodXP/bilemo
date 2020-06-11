1a640d8530fd0e16290cfc8abba3228bc5e019f9
Greg <gregory.barile@gmail.com>
2020-06-11 16:32:53 +0200

    Update UserController removeUser() to sync with database

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,7 +17,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class UserController extends AbstractController
 {
     /**
-     *@Route("/api/users", name="api_users_list", methods={"GET"})
+     * usersList
+     * @Route("/api/users", name="api_users_list", methods={"GET"})
+     * @param  mixed $request
+     * @return void
      */
     public function usersList(Request $request)
     {
@@ -35,9 +38,12 @@ class UserController extends AbstractController
 
         return $this->json($response, 200, [], ['groups' => 'users-list']);
     }
-
+   
     /**
-     *@Route("/api/users/{id}", name="api_user_details", methods={"GET"})
+     * userDetails
+     * @Route("/api/users/{id}", name="api_user_details", methods={"GET"})
+     * @param  mixed $user
+     * @return void
      */
     public function userDetails(User $user)
     {
@@ -48,9 +54,14 @@ class UserController extends AbstractController
 
         return $this->json($response, 200, [], ['groups' => 'user-details']);
     }
-
+ 
     /**
-     *@Route("/api/users", name="api_add_user", methods={"POST"})
+     * addUser
+     * @Route("/api/users", name="api_add_user", methods={"POST"})
+     * @param  mixed $request
+     * @param  mixed $serializer
+     * @param  mixed $validator
+     * @return void
      */
     public function addUser(Request $request, SerializerInterface $serializer, ValidatorInterface $validator)
     {
@@ -92,6 +103,7 @@ class UserController extends AbstractController
         $manager->flush();
         
         $response = [
+            'hypermedia' => 'There will be some links',
             'message' => 'New user added',
             'user' => $user
         ];
@@ -100,15 +112,37 @@ class UserController extends AbstractController
     }
 
     /**
-     *@Route("/api/users/{id}", name="api_remove_user", methods={"DELETE"})
+     * removeUser
+     * @Route("/api/users/{id}", name="api_remove_user", methods={"DELETE"})
+     * @param  mixed $request
+     * @param  mixed $user
+     * @return void
      */
-    public function removeUser(User $user)
+    public function removeUser(Request $request, User $user)
     {
-        $body = [
-            'message' => 'User removed',
-            'user' => $user
+        $manager = $this->getDoctrine()->getManager();
+
+        //Compares the company of the requester with the company the user belongs to.
+        $companyId = $request->query->get('companyId');
+
+        if ($companyId != $user->getCompany()->getId()) {
+
+            //If the user doesn't belong to the company, throws exception
+            throw new Exception('You are not allowed to remove a user that doesn\'t belog to your company');
+        }
+
+        //Stores the user id to be implemented in the message
+        $userId = $user->getId();
+
+        $manager->remove($user);
+
+        $manager->flush();
+
+        $response = [
+            'hypermedia' => 'There will be some links',
+            'message' => 'User '.$userId.' removed',
         ];
         
-        return $this->json($body, 200, [], ['groups' => 'users-list']);
+        return $this->json($response, 200, [], ['groups' => 'users-list']);
     }
 }
