0d2a57b78f42eda624d6b7989becbbedd33e8d20
Greg <gregory.barile@gmail.com>
2020-06-12 14:43:17 +0200

    Update deployment scripts to factorize

--- scripts/clear-project-prod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Removes all files and subfolders ##
-rm -rf /var/www/bilemo/*
-
-## Find all hidden files and delete it ##
-find /var/www/bilemo/ -maxdepth 1 -type f -name ".*" -delete
